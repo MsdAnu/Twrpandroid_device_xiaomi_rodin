3b53e8e29af953cfc3974cb464d43e0725bbe855
MsdAnu <19555a0239@gmail.com>
2026-07-01 10:32:53 +0530

    Disable Mediatek overlay and related product packages
    Comment out overlay inheritance and related packages.

--- device.mk
+++ device.mk
@@ -304,14 +304,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.nxp.mifare.xml
 
 # Overlays
-$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
-PRODUCT_PACKAGES += \
-    FrameworksResOverlayRodin \
-    NfcOverlayRodin \
-    SettingsResOverlayRodin \
-    SystemUIOverlayRodin \
-    TetheringConfigOverlay \
-    WifiOverlay
+#$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
+#PRODUCT_PACKAGES += \
+#    FrameworksResOverlayRodin \
+#    NfcOverlayRodin \
+#    SettingsResOverlayRodin \
+#    SystemUIOverlayRodin \
+#    TetheringConfigOverlay \
+#    WifiOverlay
 
 # Parts
 PRODUCT_PACKAGES += \
